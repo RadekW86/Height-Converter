2768963bb9cdd50834ee1bba43976a9a1e3f1368
Radek Włodarski <wlodarski.radek@gmail.com>
2023-02-20 21:51:09 +0100

    script update - functions

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
-console.log("Hi fellow devs!");
-let feetElement = document.querySelector(".js-feet");
-let inchesElement = document.querySelector(".js-inches");
-let centimetersElement = document.querySelector(".js-centimeters");
-feetElement.addEventListener("input", () => {
-    centimetersElement.value = "";
-});
-inchesElement.addEventListener("input", () => {
-    centimetersElement.value = "";
-});
-let formElement = document.querySelector(".js-form");
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let cm = feetElement.value * 30.48 + inchesElement.value * 2.54;
-    centimetersElement.value = cm.toFixed(2);
-});
-
-let buttonListElement = document.querySelector(".js-buttonList");
-let tableElement = document.querySelector(".table");
-buttonListElement.addEventListener("click", () => {
-    tableElement.classList.toggle("table--hidden");
-    if (tableElement.classList.contains("table--hidden")) {
-        buttonListElement.innerHTML = "ˇˇˇ Show Table ˇˇˇ";
+{
+    const welcome = () => {
+        console.log("Hi!");
     }
-    else {
-    buttonListElement.innerHTML = "ˆˆˆ Hide Table ˆˆˆ";
-    };
-});
+    welcome();
+}
+
+
+{
+    function ConvertHeight() {
+        const feetElement = document.querySelector(".js-feet");
+        const inchesElement = document.querySelector(".js-inches");
+        const centimetersElement = document.querySelector(".js-centimeters");
+        feetElement.addEventListener("input", () => {
+            centimetersElement.value = "";
+        });
+        inchesElement.addEventListener("input", () => {
+            centimetersElement.value = "";
+        });
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const cm = feetElement.value * 30.48 + inchesElement.value * 2.54;
+            centimetersElement.value = cm.toFixed(2);
+        });
+    }
+    ConvertHeight();
+}
+
+{
+    function ShowHideList() {
+        const buttonListElement = document.querySelector(".js-buttonList");
+        const tableElement = document.querySelector(".table");
+        buttonListElement.addEventListener("click", () => {
+            tableElement.classList.toggle("table--hidden");
+            if (tableElement.classList.contains("table--hidden")) {
+                buttonListElement.innerHTML = "ˇˇˇ Show Table ˇˇˇ";
+            }
+            else {
+                buttonListElement.innerHTML = "ˆˆˆ Hide Table ˆˆˆ";
+            };
+        });
+    }
+    ShowHideList();
+}
